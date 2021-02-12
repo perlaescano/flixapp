4dbfa6fd528b4508b56af901d9f5a4ef6be843af
Perla Escano <perlaescanoe@gmail.com>
2021-02-11 19:44:28 -0500

    Initial Commit

---
+++ flixapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixapp
+//
+//  Created by Perla Escano Estrella on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
