fb22eddd098843f43d6bf9663ae4ebb1df5bfaf3
Perla Escano <perlaescanoe@gmail.com>
2021-02-12 22:46:57 -0500

    MovieCell

---
+++ flixapp/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixapp
+//
+//  Created by Perla Escano Estrella on 2/12/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
