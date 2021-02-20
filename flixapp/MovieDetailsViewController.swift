9321bf2531c1ef50ce5870a9262685ea482e17df
Perla Escano <perlaescanoe@gmail.com>
2021-02-19 22:00:14 -0500

    movie details screen

--- flixapp/MovieDetailsViewController.swift
+++ flixapp/MovieDetailsViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
 
-    var movie: [String:Any]!
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,15 @@ class MovieDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+            synopsisLabel.text = movie["overview"] as? String
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+       posterView.af.setImage(withURL: posterUrl!)
+        
     }
     
 
--- flixapp/MoviesViewController.swift
+++ flixapp/MoviesViewController.swift
@@ -87,7 +87,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         //Pass the selected movie to the details view controller
         let detailsViewController = segue.destination as! MovieDetailsViewController
         detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+        
 
 }
